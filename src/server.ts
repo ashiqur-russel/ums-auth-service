f9abdd8eb4c6c558cc2aafc45969c640842d7e39
Ashiqur <ashiqur.rahman@bidi.one>
2023-09-22 00:52:52 +0200

    db connect established with config file

---
+++ src/server.ts
@@ -0,0 +1,19 @@
+import mongoose from "mongoose";
+import config from './config/index'
+
+import app from "./app"
+
+async function dbConnect() {
+    try {
+      await mongoose.connect(config.database_url as string)
+      console.log('Database Connected Successfully!')
+  
+      app.listen(config.port, () => {
+        console.log(`Example app listening on port ${config.port}`)
+      })
+    } catch (err) {
+      console.log('Failed to connect Database!', err)
+    }
+  }
+
+  dbConnect();
